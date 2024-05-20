1af052665b6415c8b3d19c7918052a7400300def
DEIRIl <153659139+DEIRIl@users.noreply.github.com>
2024-05-20 17:06:15 +0300

    123

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from random import randint, shuffle
+from random import randint, choice, shuffle
 
 
 def gen_lvl1(k=1, b=0):
@@ -17,16 +17,37 @@ def gen_lvl1(k=1, b=0):
         return f'{k if k != 1 else ""}x = {k * x}', x
 
 
-def lvl2_module1():
-    x1, x2 = randint(-10, 10), randint(-10, 10)
+def gen_lvl2_module2(c=0):
+    if c:
+        x1, x2 = randint(-10, 10), randint(-10, 10)
+        while x1 == x2:
+            x1, x2 = randint(-10, 10), randint(-10, 10)
+    else:
+        x1, x2 = 0, randint(-10, 10)
+        while x1 == x2:
+            x2 = randint(-10, 10)
 
-    a = randint(-10, 10)
+    a = randint(-3, 3)
     while a == 0:
-        a = randint(-10, 10)
+        a = randint(-3, 3)
     b = -a * (x1 + x2)
     c = a * (x1 * x2)
-    return (f'{a}x²{"+" + str(b) if b > 0 else "-" + str(-b) if b != 0 else ""}x'
-            f'{"+" + str(c) if c > 0 else "-" + str(-c) if c != 0 else ""} = 0')
+    return (
+        f'{"" if a > 0 else "-" if abs(a) == 1 else a}x²{"+" + str(b) if b > 0 else "-" + str(-b) if b != 0 else "+"}x'
+        f'{"+" + str(c) if c > 0 else "-" + str(-c) if c != 0 else ""}=0'), (x1, x2)
 
 
-print(lvl2_module1())
+def gen_lvl2_module1():
+    x = randint(1, 10)
+    if choice([True, False]):
+        return f'x²-{x ** 2}=0', (x, -x)
+    else:
+        g = randint(-(x ** 2), x ** 2)
+        while g == 0 or g == x ** 2:
+            g = randint(-(x ** 2), x ** 2)
+        r = [g, x ** 2 - g]
+        shuffle(r)
+        return f'x²{-r[0] if -r[0] < 0 else "+" + str(-r[0])}={r[1]}', (x, -x)
+
+
+print(gen_lvl2_module2())
